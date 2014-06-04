d0c33cc14cc8e0b7f9d9fc65ff78b3a29491a18d
Johannes Unterstein <unterstein@me.com>
2014-06-04 20:06:55 +0200

    possibly new index structure

--- setup/createIndex.sh
+++ setup/createIndex.sh
@@ -1,20 +1,35 @@
+#!/bin/sh
+#curl -X POST http://localhost:9200/documentsearch -d '{
+#  "mappings" : {
+#    "document" : {
+#      "properties" : {
+#        "content" : {
+#          "type" : "attachment",
+#          "fields" : {
+#            "content"  : { "store" : "yes" },
+#            "author"   : { "store" : "yes" },
+#            "title"    : { "store" : "yes" },
+#            "date"     : { "store" : "yes" },
+#            "keywords" : { "store" : "yes", "analyzer" : "keyword" },
+#            "name"    : { "store" : "yes" },
+#            "content_type" : { "store" : "yes" },
+#            "content_length" : { "store" : "yes" }
+#          }
+#        }
+#      }
+#    }
+#  }
+#}'
+
+curl -X DELETE http://localhost:9200/documentsearch
 curl -X POST http://localhost:9200/documentsearch -d '{
   "mappings" : {
     "document" : {
       "properties" : {
-        "content" : {
-          "type" : "attachment",
-          "fields" : {
-            "content"  : { "store" : "yes" },
-            "author"   : { "store" : "yes" },
-            "title"    : { "store" : "yes" },
-            "date"     : { "store" : "yes" },
-            "keywords" : { "store" : "yes", "analyzer" : "keyword" },
-            "name"    : { "store" : "yes" },
-            "content_type" : { "store" : "yes" },
-            "content_length" : { "store" : "yes" }
-          }
-        }
+        "file"  : { "type": "string", "store" : "yes", "analyzer": "german" },
+        "folder"  : { "type": "string", "store" : "yes", "analyzer": "german" },
+        "content"  : { "type": "string", "store" : "yes", "analyzer": "german" },
+        "attributes"  : { "type": "string", "store" : "yes", "analyzer": "german" }
       }
     }
   }
