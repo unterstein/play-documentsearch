c80dd675618f120fea162515c9d4c2d81f6d4e32
Johannes Unterstein <unterstein@me.com>
2014-06-04 09:56:34 +0200

    refactored classname

--- app/controllers/ApplicationController.scala
+++ app/controllers/ApplicationController.scala
@@ -2,7 +2,7 @@ package controllers
 
 import play.api.mvc._
 
-object Application extends Controller {
+object ApplicationController extends Controller {
 
   def index = Action {
     implicit request =>
