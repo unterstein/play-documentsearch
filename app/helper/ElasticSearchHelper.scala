f31b1b0925c4044e5f89be12576445102d15d932
Johannes Unterstein <unterstein@me.com>
2014-06-04 13:15:12 +0200

    sync

--- app/helper/ElasticSearchHelper.scala
+++ app/helper/ElasticSearchHelper.scala
@@ -2,6 +2,13 @@ package helper
 
 import org.slf4j.{LoggerFactory, Logger}
 import org.elasticsearch.node.NodeBuilder._
+import javax.xml.bind.DatatypeConverter
+import global.Global
+import java.io.File
+import org.elasticsearch.action.index.IndexRequestBuilder
+import org.elasticsearch.common.xcontent.XContentFactory._
+import org.apache.commons.io.FileUtils
+
 
 object ElasticSearchHelper {
 
@@ -9,12 +16,50 @@ object ElasticSearchHelper {
 
   private val client = nodeBuilder().local(true).node().client()
 
-  def sync(): Unit = {
+  private val index = "documentsearch"
 
+  def sync(): Unit = {
+    // delete old index
+    client.prepareDelete().setIndex(index).execute().actionGet()
+    // re index
+    val actions = handleFile(new File(Global.documentFolder))
+  }
+
+  private def handleFile(file: File): List[IndexRequestBuilder] = {
+    if (file.exists()) {
+      if (file.isDirectory) {
+        file.listFiles().map {
+          f =>
+            handleFile(f)
+        }.flatten.toList
+      } else {
+        // is file
+        List(client.prepareIndex()
+          .setIndex(index)
+          .setSource(// _body
+            jsonBuilder()
+              .startObject()
+              .field("_name", file.getName)
+              .field("_folder", cleanFolderName(file))
+              .field("content", base64(file))
+              .endObject()
+          )
+        )
+      }
+    } else {
+      List() // empty
+    }
   }
 
   def close(): Unit = {
     client.close()
   }
 
+  private def cleanFolderName(file: File): String = {
+    file.getParentFile.getAbsolutePath.replace(Global.documentFolder, "")
+  }
+
+  private def base64(file: File): String = {
+    DatatypeConverter.printBase64Binary(FileUtils.readFileToByteArray(file)).replace("\n", "")
+  }
 }
